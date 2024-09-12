5c57d516589aeac44454eabcfa2946496a5f2a8a
Georgi Malinov <gogotkd24@gmail.com>
2024-09-12 21:40:20 +0300

    Implemented IsValid, DiscoverDbSets, Persist methods

--- MiniORM/DbContext.cs
+++ MiniORM/DbContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace MiniORM
 {
@@ -22,7 +23,7 @@ namespace MiniORM
         {
             _connection = new DatabaseConnection(connectionString);
             _dbsetProperties = DiscoverDbSets();
-            using(new ConnectionManager(_connection))
+            using (new ConnectionManager(_connection))
             {
                 InitializeDbSets();
             }
@@ -34,16 +35,16 @@ namespace MiniORM
             var dbSets = _dbSetProperties
                 .Select(pi => pi.Value.GetValue(this))
                 .toArray();
-            
-            foreach(IEnumerable<object> dbSet in dbSets)
+
+            foreach (IEnumerable<object> dbSet in dbSets)
             {
                 var invalidEntities = dbSet
-                    .Where(entity => !isObjectValid(entity))
+                    .Where(entity => !IsValid(entity))
                     .ToArray();
 
                 if (invalidEntities.Any())
                 {
-                    throw new InvalidOperationException($"{invalidEntities.Lenghth} invalid Entities Found in {dbSet.GetType().Name}");
+                    throw new InvalidOperationException($"{invalidEntities.Length} invalid Entities Found in {dbSet.GetType().Name}");
                 }
             }
 
@@ -51,7 +52,7 @@ namespace MiniORM
             {
                 using (var transaction = _connection.StartTransaction())
                 {
-                    foreach(IEnumerable dbSet in dbSets)
+                    foreach (IEnumerable dbSet in dbSets)
                     {
                         var persistMethod = typeof(DbContext)
                             .GetMethod("Persist", BindingFlags.NonPublic | BindingFlags.Instance)
@@ -77,7 +78,51 @@ namespace MiniORM
                     transaction.Commit();
                 }
             }
-            
+
+        }
+
+        private void Persist<T>(DbSet<T> dbSet)
+            where T : class, new()
+        {
+            var entityType = typeof(T);
+            var tableName = GetTableName(entityType);
+            var columnNames = GetColumnNames(tableName).ToArray();
+
+            if (dbSet.ChangeTracker.Added.Count > 0)
+            {
+                this._connection.InsertEntities(dbSet.ChangeTracker.Added, tableName, columnNames);
+            }
+
+            var modifiedEntities = dbSet.ChangeTracker.GetModifiedEntities(dbSet).ToArray();
+            if (modifiedEntities.Length > 0)
+            {
+                this._connection.UpdateEntities(modifiedEntities, tableName, columnNames);
+            }
+
+            if (dbSet.ChangeTracker.Removed.Count > 0)
+            {
+                this._connection.DeleteEntities(dbSet.ChangeTracker.Removed, tableName, columnNames);
+            }
+        }
+
+        private static bool IsValid(object entity)
+        {
+            var validationContext = new ValidationContext(entity);
+            return Validator.TryValidateObject(entity, validationContext, null);
+        }
+
+        private IDictionary<Type, PropertyInfo> DiscoverDbSets()
+        {
+            var dbSetProperties = this.GetType().GetProperties()
+                .Where(pi => pi.PropertyType.IsGenericType && pi.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
+
+            var result = new Dictionary<Type, PropertyInfo>();
+            foreach (var property in dbSetProperties)
+            {
+                var entityType = property.PropertyType.GenericTypeArguments[0];
+                result[entityType] = property;
+            }
+            return result;
         }
     }
 }
